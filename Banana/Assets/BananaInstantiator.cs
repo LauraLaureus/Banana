12d908fde1467374152189e7fc08be6c014bbaf7
Laura <lauradelpinodiaz94@gmail.com>
2017-01-17 22:03:11 +0100

    Banana instatiated in vertical (#4)
    Also arraged initial terrain

---
+++ Banana/Assets/BananaInstantiator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaInstantiator : MonoBehaviour {
+
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+		GameObject.Instantiate (prefab, this.transform.position, Quaternion.Euler (new Vector3 (90, 0, 0)));
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
